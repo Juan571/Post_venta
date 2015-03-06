1dea549c13dba5e0c92b96ebae6443fec350d439
William Cabrera <william@linux.es>
2015-03-06 09:57:28 -0430

    comentado menu de administración

--- modulos/administracion/inventario.php
+++ modulos/administracion/inventario.php
@@ -1,11 +1,13 @@
 <?php include "../../includes/header.php"; ?>
         <script src="http://rec.vtelca.gob.ve/datatables/1.10.2/media/js/jquery.dataTables.min.js"></script>
         <link rel="stylesheet" href="http://rec.vtelca.gob.ve/datatables/1.10.2/media/css/jquery.dataTables.css">
+        <script src="http://rec.vtelca.gob.ve/reveal/1.0/jquery.reveal.js"></script>
+        <link rel="stylesheet" href="http://rec.vtelca.gob.ve/reveal/1.0/reveal.css">
         <script src="js/inventario.js"></script>
         <div class="container">
             <h1>Inventario</h1>
             <div class="row">
-                <button class="btn btn-success" id="btnAgregar"><span class="glyphicon glyphicon-plus"></span> Agregar</button>
+                <a href="#" class="btn btn-success" id="btnAgregar" data-reveal-id="myModal"><span class="glyphicon glyphicon-plus"></span> Agregar</a>
             </div>
             <hr>
             <table  class="table table-striped" id="tabla">
@@ -19,4 +21,9 @@
                 </tbody>
             </table>
         </div>
+		<div id="myModal" class="reveal-modal">
+			<h1>Reveal Modal Goodness</h1>
+			<p>This is a default modal in all its glory, but any of the styles here can easily be changed in the CSS.</p>
+			<a class="close-reveal-modal">&#215;</a>
+		</div>
 <?php include "../../includes/footer.php"; ?>
