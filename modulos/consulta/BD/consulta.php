37be624d2712667be9ad159fcf62d3fa260a341d
Pedro Lugo <robertty55@gmail.com>
2015-03-25 13:55:05 -0430

    Se culminó el prototipo para consulta de casos.

--- modulos/consulta/BD/consulta.php
+++ modulos/consulta/BD/consulta.php
@@ -3,59 +3,85 @@
     $json=array();
     $arrayCaso=array();
     $arrayAccesorios=array();
+    $arrayTrazabilidad=array();
+    $arrayError=array();
     $caso=intval($_GET["caso"]);
     $sql="SELECT
-          s_a.id as id_caso,
-          usr.nombres as nombres_operador,
-          sol.nombres nombres_cliente,
-          sol.telefono_fijo as tel_fijo,
-          sol.telefono_movil as movil,
-          sol.cedula as cedula,
-          sol.direccion as direccion,
-          modelos.modelo as modelo,
-          s_a.fecha_solicitud as fecha_solicitud,
-          eq.fecha_compra as fecha_compra,
-          op.operadora as operadora,
-          inv.producto as producto,
-          t_p.descripcion as tipo_producto,
-          m_r.motivo as motivo,
-          s_a_i.id as id_accesorio,
-          t_e.estado
-        FROM solicitudes_accesorios s_a
-        INNER JOIN tipos_estados t_e on t_e.id=tipo_estado_id
-        INNER JOIN usuarios usr on usr.id=usuario_id
-        INNER JOIN equipos eq on eq.id=equipo_id
-        INNER JOIN solicitantes sol on sol.id=eq.solicitante_id
-        INNER JOIN modelos on modelos.id=eq.modelo_id
-        INNER JOIN tecnologias tec on tec.id=eq.tecnologia_id
-        INNER JOIN operadoras op on op.id=eq.operadora_id
-        INNER JOIN solicitudes_accesorios_inventario s_a_i on s_a_i.solicitud_accesorios_id=s_a.id
-        INNER JOIN inventario inv on inv.id=s_a_i.inventario_id
-        INNER JOIN tipos_productos t_p on t_p.id=inv.tipo_producto_id
-        INNER JOIN motivos_reemplazo m_r on m_r.id=s_a_i.motivo_id WHERE s_a.id=$caso;";
+      s_a.id as id_caso,
+      CONCAT (usr.nombres, ' ', usr.apellidos) as nombres_operador,
+      CONCAT(sol.nombres, ' ', sol.apellidos) as nombres_cliente,
+      sol.telefono_fijo as tel_fijo,
+      sol.telefono_movil as movil,
+      sol.cedula as cedula,
+      sol.direccion as direccion,
+      modelos.modelo as modelo,
+      s_a.fecha_solicitud as fecha_solicitud,
+      eq.fecha_compra as fecha_compra,
+      op.operadora as operadora,
+      inv.producto as producto,
+      t_p.descripcion as tipo_producto,
+      m_r.motivo as motivo,
+      s_a_i.id as id_accesorio,
+      t_e.estado,
+      eq.factura as factura,
+      eq.imei,
+      col.color,
+      seg.fecha as fecha_seguimiento,
+      t_p1.estado as estado_seguimiento,
+      s_a_i.aprobado as aprobado
+    FROM solicitudes_accesorios s_a
+      INNER JOIN tipos_estados t_e on t_e.id=tipo_estado_id
+      INNER JOIN usuarios usr on usr.id=usuario_id
+      INNER JOIN equipos eq on eq.id=equipo_id
+      INNER JOIN solicitantes sol on sol.id=eq.solicitante_id
+      INNER JOIN modelos on modelos.id=eq.modelo_id
+      INNER JOIN tecnologias tec on tec.id=eq.tecnologia_id
+      INNER JOIN operadoras op on op.id=eq.operadora_id
+      INNER JOIN solicitudes_accesorios_inventario s_a_i on s_a_i.solicitud_accesorios_id=s_a.id
+      INNER JOIN inventario inv on inv.id=s_a_i.inventario_id
+      INNER JOIN tipos_productos t_p on t_p.id=inv.tipo_producto_id
+      INNER JOIN motivos_reemplazo m_r on m_r.id=s_a_i.motivo_id
+      INNER JOIN colores col on col.id=eq.color_id
+      INNER JOIN seguimientos seg on (s_a.id=seg.solicitudes_accesorios_id and seg.solicitudes_accesorios_inventario_id=0)
+      INNER JOIN tipos_estados t_p1 on t_p1.id=seg.tipo_estado_id where s_a.id=$caso";
     $resDatos=$hAccesorios->prepare($sql);
     $resDatos->execute();
-    $datos=$resDatos->fetchAll();
-    foreach ($datos as $k=>$v){
-        $arrayCaso[$v["id_caso"]]["nombre_operador"]=$v["nombres_operador"];
-        $arrayCaso[$v["id_caso"]]["nombres_cliente"]=$v["nombres_cliente"];
-        $arrayCaso[$v["id_caso"]]["id"]=$v["id_caso"];
-        $arrayCaso[$v["id_caso"]]["tel_fijo"]=$v["tel_fijo"];
-        $arrayCaso[$v["id_caso"]]["tel_movil"]=$v["movil"];
-        $arrayCaso[$v["id_caso"]]["cedula_cliente"]=$v["cedula"];
-        $arrayCaso[$v["id_caso"]]["direccion"]=$v["direccion"];
-        $arrayCaso[$v["id_caso"]]["modelo"]=$v["modelo"];
-        $arrayCaso[$v["id_caso"]]["fecha_solicitud"]=$v["fecha_solicitud"];
-        $arrayCaso[$v["id_caso"]]["fecha_compra"]=$v["fecha_compra"];
-        $arrayCaso[$v["id_caso"]]["operadora"]=$v["operadora"];
-        $arrayCaso[$v["id_caso"]]["estado"]=$v["estado"];
+    $cantidad=$resDatos->rowCount();
+    if ($cantidad){
+        $datos=$resDatos->fetchAll();
+        foreach ($datos as $k=>$v){
+            $arrayCaso[$v["id_caso"]]["nombre_operador"]=$v["nombres_operador"];
+            $arrayCaso[$v["id_caso"]]["nombres_cliente"]=$v["nombres_cliente"];
+            $arrayCaso[$v["id_caso"]]["id"]=$v["id_caso"];
+            $arrayCaso[$v["id_caso"]]["tel_fijo"]=$v["tel_fijo"];
+            $arrayCaso[$v["id_caso"]]["tel_movil"]=$v["movil"];
+            $arrayCaso[$v["id_caso"]]["cedula_cliente"]=$v["cedula"];
+            $arrayCaso[$v["id_caso"]]["direccion"]=$v["direccion"];
+            $arrayCaso[$v["id_caso"]]["modelo"]=$v["modelo"];
+            $arrayCaso[$v["id_caso"]]["fecha_solicitud"]=$v["fecha_solicitud"];
+            $arrayCaso[$v["id_caso"]]["fecha_compra"]=$v["fecha_compra"];
+            $arrayCaso[$v["id_caso"]]["operadora"]=$v["operadora"];
+            $arrayCaso[$v["id_caso"]]["estado"]=$v["estado"];
+            $arrayCaso[$v["id_caso"]]["imei"]=$v["imei"];
+            $arrayCaso[$v["id_caso"]]["color"]=$v["color"];
+            $arrayCaso[$v["id_caso"]]["factura"]=$v["factura"];
+        }
+        foreach ($datos as $k=>$v){
+            $arrayAccesorios[$v["id_accesorio"]]["producto"]=$v["producto"];
+            $arrayAccesorios[$v["id_accesorio"]]["tipo_producto"]=$v["tipo_producto"];
+            $arrayAccesorios[$v["id_accesorio"]]["motivo"]=$v["motivo"];
+            $arrayAccesorios[$v["id_accesorio"]]["aprobado"]=$v["aprobado"];
+        }
+        foreach($datos as $k=>$v){
+            $arrayTrazabilidad[$v["estado_seguimiento"]]["estado_seguimiento"]=str_replace(" ", "_", $v["estado_seguimiento"]);
+            $arrayTrazabilidad[$v["estado_seguimiento"]]["fecha_seguimiento"]=$v["fecha_seguimiento"];
+        }
+        $json["caso"]=$arrayCaso;
+        $json["accesorios"]=$arrayAccesorios;
+        $json["trazabilidad"]=$arrayTrazabilidad;
+        print json_encode($json);
+    }else{
+        $json["error"]="No existe el código ingresado, intente de nuevo";
+        print json_encode($json);
     }
-    foreach ($datos as $k=>$v){
-        $arrayAccesorios[$v["id_accesorio"]]["producto"]=$v["producto"];
-        $arrayAccesorios[$v["id_accesorio"]]["tipo_producto"]=$v["tipo_producto"];
-        $arrayAccesorios[$v["id_accesorio"]]["motivo"]=$v["motivo"];
-    }
-    $json["caso"]=$arrayCaso;
-    $json["accesorios"]=$arrayAccesorios;
-    print json_encode($json);
 ?>
--- modulos/consulta/js/eventos.js
+++ modulos/consulta/js/eventos.js
@@ -1,13 +1,97 @@
 $(document).ready(function(){
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
     //$("#contCaso").hide();
+    $("#contenidoConsulta").hide();
     $("#buscarCaso").click(function(){
+        $("#contenidoConsulta").fadeOut();
+        aaa={};
         idCaso=$("#idCaso").val();
         $.getJSON('BD/consulta.php?caso='+idCaso, function(json) {
-            console.log(json);
-            strHTML="";
-            strHTML+="<tr>";
-
-            strHTML+="</tr>";
+            strCaso="";
+            strCliente="";
+            strEquipo="";
+            if (json.error){
+                alert(json.error)
+            }else{
+                id_caso="";
+                nombre_operador="";
+                fecha_solicitud="";
+                estado="";
+                cedula_cliente="";
+                nombre_cliente="";
+                direccion="";
+                tel_fijo="";
+                tel_movil="";
+                imei="";
+                modelo="";
+                color="";
+                factura="";
+                fecha_compra="";
+                operadora="";
+                aaa=json;
+                $.each(json["caso"], function(i, j){
+                    id_caso=j["id"];
+                    nombre_operador=j["nombre_operador"];
+                    estado=j["estado"];
+                    fecha_solicitud=j["fecha_solicitud"];
+                    cedula_cliente=j["cedula_cliente"];
+                    nombre_cliente=j["nombres_cliente"];
+                    direccion=j["direccion"];
+                    tel_fijo=j["tel_fijo"];
+                    tel_movil=j["tel_movil"];
+                    imei=j["imei"];
+                    modelo=j["modelo"];
+                    color=j["color"];
+                    factura=j["factura"];
+                    fecha_compra=j["fecha_compra"];
+                    operadora=j["operadora"];
+                });
+                strCaso='<div class="row"><div class="col-md-12"><h2>Datos del caso</h2></div></div>' +
+                '<div class="row"><div class="col-md-3" style="text-align: right"><b>Caso N°</b></div><div class="col-md-3">'+id_caso+'</div>' +
+                '<div class="col-md-3" style="text-align: right"><b>Fecha de Solicitud</b></div><div class="col-md-3">'+fecha_solicitud+'</div></div>' +
+                '<div class="row"><div class="col-md-3" style="text-align: right"><b>Operador</b></div><div class="col-md-3">'+nombre_operador+'</div>' +
+                '<div class="col-md-3" style="text-align: right"><b>Estatus</b></div><div class="col-md-3">'+estado+'</div></div>';
+                $("#contCaso").html(strCaso);
+                strCliente='<div class="row"><div class="col-md-12"><h2>Datos del cliente</h2></div></div>' +
+                '<div class="row"><div class="col-md-3" style="text-align: right"><b>Cédula</b></div><div class="col-md-3">'+cedula_cliente+'</div>' +
+                '<div class="col-md-3" style="text-align: right"><b>Nombre</b></div><div class="col-md-3">'+nombre_cliente+'</div></div>' +
+                '<div class="row"><div class="col-md-3" style="text-align: right"><b>Dirección</b></div><div class="col-md-9">'+direccion+'</div>' +
+                '</div><div class="row"><div class="col-md-3" style="text-align: right"><b>Telefono Fijo</b></div><div class="col-md-3">'+tel_fijo+'</div>' +
+                '<div class="col-md-3" style="text-align: right"><b>Telefono Movil</b></div><div class="col-md-3">'+tel_movil+'</div>';
+                $("#datos_cliente").html(strCliente);
+                strEquipo='<div class="row"><div class="col-md-12"><h2>Datos del equipo</h2></div></div>' +
+                '<div class="row"><div class="col-md-2" style="text-align: right"><b>IMEI</b></div><div class="col-md-2">'+imei+'</div>' +
+                '<div class="col-md-2" style="text-align: right"><b>Modelo</b></div><div class="col-md-2">'+modelo+'</div>' +
+                '<div class="col-md-2" style="text-align: right"><b>Color</b></div><div class="col-md-2">'+color+'</div></div>' +
+                '<div class="row"><div class="col-md-2" style="text-align: right"><b>N° de Factura</b></div><div class="col-md-2">'+factura+'</div>' +
+                '<div class="col-md-2" style="text-align: right"><b>Fecha de Compra</b></div><div class="col-md-2">'+fecha_compra+'</div>' +
+                '<div class="col-md-2" style="text-align: right"><b>Operadora</b></div><div class="col-md-2">'+operadora+'</div></div>';
+                $("#datos_equipo").html(strEquipo);
+                bodyAccesorios="";
+                $.each(json["accesorios"], function(i, j){
+                    aprobado="";
+                    bodyAccesorios+="<tr>";
+                        bodyAccesorios+="<td>"+j["producto"]+"</td>";
+                        bodyAccesorios+="<td>"+j["tipo_producto"]+"</td>";
+                        bodyAccesorios+="<td>"+j["motivo"]+"</td>";
+                        aprobado=(j["aprobado"]==1)?"<span class='glyphicon glyphicon-ok'></span>":"<span class='glyphicon glyphicon-remove'></span>";
+                        bodyAccesorios+="<td>"+aprobado+"</td>";
+                    bodyAccesorios+="</tr>";
+                });
+                $("#bodyAccesorios").html(bodyAccesorios);
+                $("#contenidoConsulta").slideDown();
+                $("#pendiente").html("--/--/-- --:--:--");
+                $("#Procesado").html("--/--/-- --:--:--");
+                $("#Despachado").html("--/--/-- --:--:--");
+                $("#En_Oficina_Comercial").html("--/--/-- --:--:--");
+                $("#Entregado").html("--/--/-- --:--:--");
+                $.each(json["trazabilidad"], function (i, j) {
+                    $("#"+j["estado_seguimiento"]).html(j["fecha_seguimiento"]);
+                    $("#"+j["estado_seguimiento"]+"-icon").html("<span class='glyphicon glyphicon-ok'></span>")
+                });
+            }
         }).fail(function(j, t){
             console.log("Per Error: " + t);
         });
