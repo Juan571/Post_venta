872234ac8670a73ca0664c48e3ba6e514f90ab6a
Juan <jromero@vtelca.gob.ve>
2015-03-26 09:17:22 -0430

    se modfica parametrosbd

--- includes/parametrosBD.php
+++ includes/parametrosBD.php
@@ -1,7 +1,7 @@
 <?php
-    define("host","localhost"); # Direccion ip Base de datos
+    define("host","192.168.200.93"); # Direccion ip Base de datos
     define("BD","postventa_accesorios");     # nombre de la base de datos
     define("SGBD","mysql");             # Sistema manegador de Base de datos  (mysql, postgresql =  pgsql)
-    define("usuario","root");     # nombre de usario conexion
-    define("contrasena","0000");     # contrasena
+    define("usuario","usr_dev");     # nombre de usario conexion
+    define("contrasena","*s3l3ct#*");     # contrasena
 ?>
